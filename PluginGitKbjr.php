5da985ea16487ff18b7b1551d54d06a6a23254dc
Claes Ö <claes@stenbergit.se>
2019-06-14 09:42:14 +0200

    Add methods add, commit, push, pull, fetch, log, exist.

--- PluginGitKbjr.php
+++ PluginGitKbjr.php
@@ -4,17 +4,13 @@ class PluginGitKbjr{
   function __construct() {
     require_once(__DIR__.'/lib/Git.php');
   }
-  public function set_repo($plugin){
-    $this->path_to_repo = wfGlobals::getAppDir().'/plugin/'.$plugin;
-  }
-  public function status(){
-    $folder_exist = wfFilesystem::fileExist($this->path_to_repo.'/.git');
-    if(!$folder_exist){
-      return null;
-    }else{
-      $repo = Git::open($this->path_to_repo);
-      $status = $repo->status();
-      return $status;
-    }
-  }
+  public function set_repo($plugin){$this->path_to_repo = wfGlobals::getAppDir().'/plugin/'.$plugin;}
+  public function exist(){return wfFilesystem::fileExist($this->path_to_repo.'/.git');}
+  public function status()         {$repo = Git::open($this->path_to_repo);                          return $repo->status();}
+  public function add()            {$repo = Git::open($this->path_to_repo); $repo->add();            return null;           }
+  public function commit($message) {$repo = Git::open($this->path_to_repo); $repo->commit($message); return null;           }
+  public function push()           {$repo = Git::open($this->path_to_repo); $repo->push();           return null;           }
+  public function pull()           {$repo = Git::open($this->path_to_repo); $repo->pull();           return null;           }
+  public function log()            {$repo = Git::open($this->path_to_repo);                          return $repo->log();   }
+  public function fetch()          {$repo = Git::open($this->path_to_repo); $repo->fetch();          return null;           }
 }
